63c5e6b7aa8d150cc44129cb4f915595e2aac5e1
limulim <l22139428@gmail.com>
2022-09-05 20:16:40 +0900

    data CRUD

--- src/test/java/org/zerock/mapper/TimeMapperTests.java
+++ src/test/java/org/zerock/mapper/TimeMapperTests.java
@@ -1,6 +1,5 @@
 package org.zerock.mapper;
 
-import org.apache.commons.logging.Log;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
